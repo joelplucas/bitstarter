aaf8f65e581542678e2aaf0e55a384b497078f8d
joelplucas <joelpl@gmail.com>
2013-07-10 17:05:35 +0000

    assignment 1 2.0

--- web.js
+++ web.js
@@ -9,9 +9,9 @@ var buf = new Buffer(fileContent);
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});ileContent);
-
+});
